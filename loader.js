2e20149f6cad89e06f1e4274813984921d517901
Impacts_Youtube <44884873+coolspotyt@users.noreply.github.com>
2026-09-17 10:59:07 -0400

    Update loader.js

--- loader.js
+++ loader.js
@@ -36,5 +36,5 @@
   });
 
   // Show the animation for three seconds unless the visitor skips it.
-  deadline = window.setTimeout(() => finish(), 3000);
+  deadline = window.setTimeout(() => finish(), 2700);
 })();
